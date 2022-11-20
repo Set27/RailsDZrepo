fa41a1c459afb2eac8fc8f67bbd6fb1877603e36
Set27 <matvei.2002@mail.ru>
2022-11-20 18:45:09 +0300

    validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
-    has_many :posts
-    has_many :comments
+    validates :login, presence: true, length {minimum: 5}, uniqueness: true
+    validates :password, presence: true, format {with: /[a-zA-Z0-9]+{6,18}/, message: 'password incorrect} 
+    has_many :posts, dependent :destroy
+    has_many :comments, dependent :destroy
 end
