a740ad92e0579af49320974103c756e183013ed8
Set27 <matvei.2002@mail.ru>
2022-12-03 17:28:32 +0300

    Add logit to sub

--- app/controllers/subscribes_controller.rb
+++ app/controllers/subscribes_controller.rb
@@ -1,9 +1,28 @@
 class SubscribesController < ApplicationController
-    def show
-        @sub = Subscribe.find(params[:id])
-    end
-
     def index
         @subs = Subscribe.all
     end
+
+    def new
+        @sub = Subscribe.new        
+    end
+
+    def create
+        @sub = Subscribe.new(sub_params)
+        
+        if @sub.subscribe_to == @sub.user_id
+            flash[:error] = "user cannot follow themself"
+        else
+            if @sub.save
+                # redirect_to subscribes_path
+            else
+                render :new, status: :unprocessable_entity
+            end
+        end
+    end
+
+    private
+        def sub_params
+            params.require(:subscribe).permit(:user_id,:subscribe_to)
+        end
 end
